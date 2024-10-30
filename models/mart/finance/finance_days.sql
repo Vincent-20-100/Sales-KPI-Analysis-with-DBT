a98699a6c9a288cd8c24c4a78521f87daeb9740b
Vincent <vincent.lamy.33@gmail.com>
2024-10-30 10:42:21 +0000

    last litles changes before merging

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -1,4 +1,4 @@
- SELECT
+SELECT
      date_date
      ,COUNT(orders_id) AS nb_transactions
      ,ROUND(SUM(revenue),0) AS revenue
@@ -11,6 +11,6 @@
      ,ROUND(SUM(logcost),0) AS logcost
      ,ROUND(SUM(ship_cost),0) AS ship_cost
      ,SUM(quantity) AS quantity
- FROM {{ref("int_orders_operational")}}
- GROUP BY  date_date
- ORDER BY  date_date DESC
+FROM {{ref("int_orders_operational")}}
+GROUP BY  date_date
+ORDER BY  date_date DESC
